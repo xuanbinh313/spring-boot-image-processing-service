b59cd42b476a02710aef2dfcb41d8931d4a40dd0
BinhTX <binh.xuantran@digicommercegroup.com>
2024-10-29 09:16:21 +0700

    first commit

---
+++ src/main/java/com/binhcodev/spring_boot_image_processing_service/SpringBootImageProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.binhcodev.spring_boot_image_processing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootImageProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootImageProcessingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binhcodev/spring_boot_image_processing_service/SpringBootImageProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binhcodev.spring_boot_image_processing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootImageProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
